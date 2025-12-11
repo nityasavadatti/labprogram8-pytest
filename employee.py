5bd6c842c260bed92d2334bf3d0ed3866765e62e
nityasavadatti <nityasavadatti39@gmail.com>
2025-12-11 15:42:13 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,7 +1,7 @@
 def  employee_details(name,emp_id,department,salary):
     result= (
         f" Employee Name: {name}\n"
-        f" Employee ID: {Emp_id}\n"
+        f" Employee ID: {emp_id}\n"
         f" Employee department: {department}\n"
         f" Employee salary: {salary}\n"
     )
@@ -9,7 +9,7 @@ def  employee_details(name,emp_id,department,salary):
 if__name__="__main__":
 #sample input(you can change)
 Name="alice"
-Emp_id="e1001"
+emp_id="e1001"
 department="IT"
 salary=55000
 print(employee_details(name,emp_id,department,salary))
