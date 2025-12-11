166c6ef934c2f9b26eed665c5f21d6fe0b65f023
nityasavadatti <nityasavadatti39@gmail.com>
2025-12-11 15:46:56 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,9 +1,9 @@
-def  employee_details(name,emp_id,department,salary):
+def employee_details(name,emp_id,department,salary):
     result= (
-        f" Employee Name: {name}\n"
-        f" Employee ID: {emp_id}\n"
-        f" Employee department: {department}\n"
-        f" Employee salary: {salary}\n"
+        f"Employee Name:{name}\n"
+        f"Employee ID:{emp_id}\n"
+        f"Employee department:{department}\n"
+        f"Employee salary:{salary}\n"
     )
     return result
 if__name__="__main__":
