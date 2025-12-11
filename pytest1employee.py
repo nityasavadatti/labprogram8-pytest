4593bc77d29a5cee1f31963c302ee76e82dec6f1
nityasavadatti <nityasavadatti39@gmail.com>
2025-12-11 15:28:17 +0530

    Update pytest1employee.py

--- pytest1employee.py
+++ pytest1employee.py
@@ -1,9 +1,9 @@
 def  employee_details(name,emp_id,department,salary):
-    result=(
-        f"Employee Name: {name}\n"
-        f"Employee ID: {Emp_id}\n"
-        f"Employee department: {department}\n"
-        f"Employee salary: {salary}\n"
+    result= (
+        f" Employee Name: {name}\n"
+        f" Employee ID: {Emp_id}\n"
+        f" Employee department: {department}\n"
+        f" Employee salary: {salary}\n"
     )
     return result
 if__name__="__main__":
